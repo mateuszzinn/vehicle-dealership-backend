61bf81337c899c8b06d6a03fcdd381602e1623b7
mateuszzinn <mateuslima2002.ml@gmail.com>
2026-08-13 08:24:02 -0300

    starting project

---
+++ src/main/java/com/example/vehicledealershipbackend/VehicleDealershipBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.vehicledealershipbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleDealershipBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleDealershipBackendApplication.class, args);
+    }
+
+}
